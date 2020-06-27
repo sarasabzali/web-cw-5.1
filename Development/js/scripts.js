d85f052a9af9202b3d4ce47f507c3100fca386ac
sarasabzali <66485122+sarasabzali@users.noreply.github.com>
2020-06-27 20:01:23 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,36 @@
 // السؤال الأول
 // 1
-
+const name ='sara';
 // 2
-
+let age =17;
 // 3 and 4
-
+console.log("انا"+" "+"name"+"وعمري"+" "+"age");
 // 5 and 6
-
+age += 5;
+console.log(age*5);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+let hobbies = ["playing","books","walking"];
 
 // 2
-
+hobbies.push("17");
 // 3
-
+hobbies.push("17");
 // 4
-
+hobbies.pop();
 // 5
 
+let best={
+    lang: "javascript",
+    name:"web",
+    TA:["menawer","moudhi","nancy","kaja"] 
+}
 // 6
-
+console.log (web.name)
 // 7
-
+console.log(web.TA[0])
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
